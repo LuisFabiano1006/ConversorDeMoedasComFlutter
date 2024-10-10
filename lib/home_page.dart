4fcb1eccf6e405d05306995e8ecc6e6343edc877
Luis Fabiano Gomes <luidibaricheli000@gmail.com>
2024-10-10 09:04:22 -0300

    campo de conversao

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,13 +10,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _primeiroNumeroController = TextEditingController();
+  final TextEditingController _segundoNumeroController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.amber,
-        title: const Center(
+        title:  Center(
           child: Text(
             'Conversor de Moedas',
             style: TextStyle(
@@ -23,6 +28,30 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      body:  Padding(
+        padding: EdgeInsets.all(16,),
+        child: Column(
+          children: [
+            TextField(
+              controller: _primeiroNumeroController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _segundoNumeroController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
